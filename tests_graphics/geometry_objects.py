ba1e2d91fc2fcd07c0cd42538dffeb51f690e55b
appteam228 <ghfdfjdkjfkdj@gmail.com>
2023-04-14 03:13:42 +0300

    all changes were added

--- tests_graphics/geometry_objects.py
+++ tests_graphics/geometry_objects.py
@@ -14,47 +14,81 @@ class File_treatmenter():
         self.data_type = data_type
         self.file_name = file_name
         self.bin_mode = bin_mode
+        if self.bin_mode == True:
+            self.file_name + ".bin"
 
     def generate_data(self):
 
-        if self.bin_mode == False:
-            if self.data_type == "normal":
-                x_cores = np.random.normal(rd.randint(0, 12), 0.76, 1000000)
-                y_cores = np.random.normal(rd.randint(0, 12), 0.76, 1000000)
-                z_cores = np.random.normal(rd.randint(0, 12), 0.76, 1000000)
-                self.basis_matrix = np.stack((x_cores, y_cores, z_cores))
+        if self.data_type == "normal":
+            x_cores = np.random.normal(rd.randint(0, 12), 0.76, 1000000)
+            y_cores = np.random.normal(rd.randint(0, 12), 0.76, 1000000)
+            z_cores = np.random.normal(rd.randint(0, 12), 0.76, 1000000)
+            self.basis_matrix = np.stack((x_cores, y_cores, z_cores))
 
-            if self.data_type == "gamma":
-                x_cores = np.random.gamma(rd.randint(0, 12), 0.76, 1000000)
-                y_cores = np.random.gamma(rd.randint(0, 12), 0.76, 1000000)
-                z_cores = np.random.gamma(rd.randint(0, 12), 0.76, 1000000)
-                self.basis_matrix = np.stack((x_cores, y_cores, z_cores))
+        if self.data_type == "gamma":
+            x_cores = np.random.gamma(rd.randint(0, 12), 0.76, 1000000)
+            y_cores = np.random.gamma(rd.randint(0, 12), 0.76, 1000000)
+            z_cores = np.random.gamma(rd.randint(0, 12), 0.76, 1000000)
+            self.basis_matrix = np.stack((x_cores, y_cores, z_cores))
 
-            if self.data_type == "chi_square":
-                x_cores = np.random.chisquare(float(rd.randint(0, 12)), 1000000)
-                y_cores = np.random.chisquare(float(rd.randint(0, 12)), 1000000)
-                z_cores = np.random.chsiquare(float(rd.randint(0, 12)), 1000000)
-                self.basis_matrix = np.stack((x_cores, y_cores, z_cores))
+        if self.data_type == "chi_square":
+            x_cores = np.random.chisquare(float(rd.randint(0, 12)), 1000000)
+            y_cores = np.random.chisquare(float(rd.randint(0, 12)), 1000000)
+            z_cores = np.random.chsiquare(float(rd.randint(0, 12)), 1000000)
+            self.basis_matrix = np.stack((x_cores, y_cores, z_cores))
             
-            if self.data_type == "stude_distrib":
-                x_cores = np.random.standard_t(float(rd.randint(0, 12)), 1000000)
-                y_cores = np.random.standard_t(float(rd.randint(0, 12)), 1000000)
-                z_cores = np.random.standard_t(float(rd.randint(0, 12)), 1000000)
-                self.basis_matrix = np.stack((x_cores, y_cores, z_cores))
+        if self.data_type == "stude_distrib":
+            x_cores = np.random.standard_t(float(rd.randint(0, 12)), 1000000)
+            y_cores = np.random.standard_t(float(rd.randint(0, 12)), 1000000)
+            z_cores = np.random.standard_t(float(rd.randint(0, 12)), 1000000)
+            self.basis_matrix = np.stack((x_cores, y_cores, z_cores))
 
-            if self.data_type == "weibul":
-                x_cores = np.random.weibull(float(rd.randint(0, 12)), 1000000)
-                y_cores = np.random.weibull(float(rd.randint(0, 12)), 1000000)
-                z_cores = np.random.weibull(float(rd.randint(0, 12)), 1000000)
-                self.basis_matrix = np.stack((x_cores, y_cores, z_cores))
+        if self.data_type == "weibul":
+            x_cores = np.random.weibull(float(rd.randint(0, 12)), 1000000)
+            y_cores = np.random.weibull(float(rd.randint(0, 12)), 1000000)
+            z_cores = np.random.weibull(float(rd.randint(0, 12)), 1000000)
+            self.basis_matrix = np.stack((x_cores, y_cores, z_cores))
             
-            if self.data_type == "fisher":
-                x_cores = np.random.f(float(rd.randint(0, 12)), 0.76, 1000000)
-                y_cores = np.random.f(float(rd.randint(0, 12)), 0.76, 1000000)
-                z_cores = np.random.f(float(rd.randint(0, 12)), 0.76, 1000000)
-                self.basis_matrix = np.stack((x_cores, y_cores, z_cores))
+        if self.data_type == "fisher":
+            x_cores = np.random.f(float(rd.randint(0, 12)), 0.76, 1000000)
+            y_cores = np.random.f(float(rd.randint(0, 12)), 0.76, 1000000)
+            z_cores = np.random.f(float(rd.randint(0, 12)), 0.76, 1000000)
+            self.basis_matrix = np.stack((x_cores, y_cores, z_cores))
         
+        if self.bin_mode == "False":
             with open(self.file_name, "w") as file:
                 for (x, y, z) in self.basis_matrix:
                     file.write(f"{x}\t{y}\t{z}\n")
-                    # продолжить сдесь
+
+        elif self.bin_mode == "Bin":
+            with open(self.file_name, "wb") as file:
+                for (x, y, z) in self.basis_matrix:
+                    file.write(f"{bin(x)}\t{bin(y)}\t{bin(z)}\n")
+    
+    def generate_ot_Array(self, ARRAY):
+        if self.bin_mode == "False":
+            with open(self.file_name, "w") as file:
+                for (x, y, z) in ARRAY:
+                    file.write(f"{x}\t{y}\t{z}\n")
+
+        elif self.bin_mode == "Bin":
+            with open(self.file_name, "wb") as file:
+                for (x, y, z) in ARRAY:
+                    file.write(f"{bin(x)}\t{bin(y)}\t{bin(z)}\n")
+    
+
+
+    
+
+class Geometry(File_treatmenter):
+
+    def __init__(self) -> None:
+        
+
+
+        
+        
+        
+                
+        
+                    
