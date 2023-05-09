804c637cd6eaa2f7fce1e37a359fc7fe49be317d
appteam228 <ghfdfjdkjfkdj@gmail.com>
2023-05-09 19:08:00 +0300

    all cahnges were added

--- main.py
+++ main.py
@@ -1,64 +1,67 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sklearn.datasets as datasets
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+import matplotlib.animation as manimation
+import matplotlib.colors as mcolors
+import pandas as pd
+import random as rd
+import os
+from progress.bar import IncrementalBar
 
+plt.style.use("seaborn")
+def generate_randin_points(P_C=10000):
 
-class Perceptron():
-    def __init__(self, eta, epochs) -> None:
-        self.eta = eta
-        self.epochs = epochs
+    x_cores, y_cores, z_cores = np.zeros(P_C + 1), np.zeros(P_C + 1), np.zeros(P_C + 1)
+    for index in range(P_C + 1):
+        x_cores[index] = x_cores[index - 1] + float(rd.randint(0, 123)) * rd.choice([-1, 1])
+        y_cores[index] = y_cores[index - 1] + float(rd.randint(0, 123)) * rd.choice([-1, 1])
+        z_cores[index] = z_cores[index - 1] + float(rd.randint(0, 123)) * rd.choice([-1, 1])
     
-    def fit(self, X, y):
+    cores = np.stack((x_cores, y_cores, z_cores)).T
+    with open("data.xyz", "w") as file:
+        with IncrementalBar(max=(P_C + 1)) as bar:
+            for (index, item) in enumerate(cores):
+                bar.next()
+                file.write(f"{item[0]}\t{item[1]}\t{item[2]}\n")
+    return cores
 
-        self.W_ = np.zeros(X.shape[1])
-        self.cost_ = []
-        for _ in range(self.epochs):
-            errors = 0
-            for (xi, true_label) in zip(X, y):
-                update = true_label - self.net_input(xi)
-                self.W_[1:] += self.eta * update * xi
-                self.W_[0] += self.eta * update
-                errors = int(update != 0.0)
-            self.cost_.append(errors)
-        return self
-    
-    def net_input(self, X):
-        return np.dot(X, self.W_[1:]) + self.W_[0]
-    
-    def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0, 1, -1)
+def graph_plane_plt(time_coef):
+
+    axis_3d.clear()
+    time = np.linspace(1, np.pi, 100)
+    x_cores = np.linspace(-np.pi, np.pi, 100)
+    y_cores = np.linspace(-np.pi, np.pi, 100)
+    xy1_grid, xy2_grid = np.meshgrid(x_cores, y_cores)
+
+    z_cores = np.sin(np.sqrt(xy1_grid ** 2 * time_coef + xy2_grid ** 2 * time_coef)) / (np.sqrt(xy1_grid ** 2 * time_coef + xy2_grid ** 2 * time_coef))
+
+    axis_3d.plot_surface(xy1_grid, xy2_grid, z_cores, cmap="bone", alpha=0.56)
+
+def graph_desion(X, Y, classifier, test_idx):
+
+    markers = ["o", "+", "v", "^"]
+    colors = ["gray", "blue", "red"]
+    cmaps = mcolors.ListedColormap(colors[:len(np.unique(Y))])
+
+    xx1_grid, xx2_grid = np.meshgrid(np.arange(X[:, 0].min() - 1, X[:, 0].max() + 1),
+                                     np.arange(X[:, 1].min() - 1, X[:, 1].max() + 1))
+    z_grid = classifier.predict(np.array[xx1_grid.ravel(), xx2_grid.ravel()]).T
+    z_grid = z_grid.reshape(xx1_grid.shape)
+    plt.contourf(xx1_grid, xx2_grid, z_grid, cmap=cmaps, alpha=0.3)
+
+    for (index, cls) in enumerate(np.unique(Y)):
+        plt.scatter(x=X[Y==cls, 0], y=X[Y==cls, 1], 
+                    marker=markers[index], color=colors(index))
 
 
-data = datasets.load_iris()
 figure = plt.figure()
-axis_3d_01 = figure.add_subplot(2, 2, 1, projection="3d")
-axis_2d_02 = figure.add_subplot(2, 2, 2)
-X = data.data[:, [0, 1, 2]]
-y = data.target
-markers = ["v", "^", "x"]
-colors = ["red", "blue", "green"]
-angles = np.linspace(-np.pi, np.pi, 3)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-snn = StandardScaler()
-snn.fit(X_train)
-X_train_std, X_test_std = snn.transform(X_train), snn.transform(X_test)
-ppn = Perceptron(epochs=40, eta=0.001)
-ppn.fit(X_train_std, y_train)
-for (index, cls) in enumerate(np.unique(y)):
-    axis_3d_01.scatter(X[y == cls, 0], X[y == cls, 1], X[y == cls, 2],
-                    color=colors[index], marker=markers[index], label=cls)
-    curent_x = X[y == cls, 0]
-    curent_y = X[y == cls, 1]
-    xx1_grid, xx2_grid = np.meshgrid(curent_x, curent_y)
-    Z = (index * xx1_grid + index * xx2_grid) / index
-    #Z_01 = np.sin(np.sqrt(xx1_grid ** 2 + xx2_grid ** 2)) / np.tanh(xx1_grid ** 2 + xx2_grid ** 2)
-    axis_3d_01.plot_surface(xx1_grid, xx2_grid, Z, color="gray", alpha=0.2)
-    #axis_3d.plot_surface(xx1_grid, xx2_grid, Z_01, color="gray", alpha=0.2)
-axis_2d_02.plot(range(0, len(ppn.cost_)), ppn.cost_, color="green", marker="x", label="[cost info]")
-
-plt.legend(loc="upper left")
+axis_3d = figure.add_subplot(1, 1, 1, projection="3d")
+array = generate_randin_points(P_C=10000)
+#axis_3d.scatter(array[:, 0], array[:, 1], array[:, 2], c=array[:, 2], cmap="twilight", s=0.78, alpha=0.3, marker="v")
+#axis_2d_01.plot(range(0, len(array[:, 0])), array[:, 0], color="red")
+animation = manimation.FuncAnimation(figure, graph_plane_plt, interval=100)
 plt.show()
+
+
+
 
 
--- tests_graphics/water_waves.py
+++ tests_graphics/water_waves.py
@@ -1,31 +1,98 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import numpy as np
-import random as rd
-import matplotlib.animation as animo
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+import sklearn.datasets as datasets
+from sklearn.linear_model import Perceptron, LogisticRegression
 
-figure = plt.figure()
-axis_3d = figure.add_subplot(projection="3d")
 
-theta = np.linspace(0, np.pi * 2, 100)
-phi = np.linspace(0, np.pi * 2, 100)
-time = np.linspace(0, np.pi, 100)
 
 plt.style.use("seaborn")
+figure = plt.figure()
+axis_3d = figure.add_subplot(2, 3, 1,projection="3d")
+axis_3d_01 = figure.add_subplot(2, 3, 2, projection="3d")
+axis_3d_02 = figure.add_subplot(2, 3, 3, projection="3d")
+axis_3d.set_xlabel("параметр [x1]")
+axis_3d.set_ylabel("параметр [x2]")
+axis_3d.set_zlabel("параметр [x3]")
+axis_3d_01.set_xlabel("параметр [x1]")
+axis_3d_01.set_ylabel("параметр [x2]")
+axis_3d_01.set_zlabel("параметр [x3]")
+axis_3d_02.set_xlabel("параметр [x1]")
+axis_3d_02.set_ylabel("параметр [x2]")
+axis_3d_02.set_zlabel("параметр [x3]")
 
-def animation(i):
-    
-    axis_3d.clear()
-    x_cores = np.linspace(-np.pi, np.pi, 100)
-    y_cores = np.linspace(-np.pi, np.pi, 100)
-    
-    xx_cores, yy_cores = np.meshgrid(x_cores, y_cores)
-    #eta = np.tanh((-1 * (xx_cores * i - 5) ** 2 / 20) - (yy_cores * i - 5) ** 2 / 100) / 2
-    eta = 123 * np.sin(xx_cores ** 2 + i + yy_cores ** 2 + i - 35)
-    axis_3d.plot_surface(xx_cores, yy_cores, eta, cmap="twilight", alpha=0.76)
-    axis_3d.contour(xx_cores, yy_cores, eta, zdir="z", offset=-0.50, cmap="coolwarm")
-    axis_3d.contour(xx_cores, yy_cores, eta, zdir="x", offset=0, cmap="coolwarm")
-    axis_3d.contour(xx_cores, yy_cores, eta, zdir="y", offset=0, cmap="coolwarm")
-    
 
-anim = animo.FuncAnimation(figure, animation, interval=100)
+def generate_desion(X, Y, classifier, test_idx=None, curent_view=axis_3d, basis=2):
+    markers = ["o", "x", "v", "^"]
+    colors = ["tomato", "blue", "gray", "green"]
+    cmaps = mcolors.ListedColormap(colors[:len(np.unique(Y))])
+    xx1_grid, xx2_grid = np.meshgrid(np.arange(X[:, 0].min() - 1, X[:, 0].max() + 1),
+                                     np.arange(X[:, 1].min() - 1, X[:, 1].max() + 1))
+    z_grid = classifier.predict(np.array([xx1_grid.ravel(), xx2_grid.ravel()]).T)
+    z_grid = z_grid.reshape(xx1_grid.shape)
+    
+    if basis == 2:
+        curent_view.contourf(xx1_grid, xx2_grid, z_grid, cmap=cmaps, alpha=0.3)
+
+        for (index, cls) in enumerate(np.unique(Y)):
+            Z_array = np.linspace(0, len(X[Y==cls, 0]))
+            curent_view.scatter(X[Y==cls, 0], X[Y==cls, 1], marker=markers[index],
+                        c=cmaps(index), alpha=0.75, s=12.23, label=cls)
+
+            if test_idx:
+                x_test, y_test = X[test_idx, :], Y[test_idx]
+                curent_view.scatter(x_test[:, 0], x_test[:, 1], linewidths=1, edgecolors="black", alpha=0.5, marker="o", label=f"test elem [{index}]")
+
+    elif basis == 3:
+        curent_view.plot_surface(xx1_grid, xx2_grid, z_grid, cmap=cmaps, alpha=0.3)
+
+        for (index, cls) in enumerate(np.unique(Y)):
+            Z_array = np.linspace(0, len(X[Y==cls, 0]))
+            curent_view.scatter(X[Y==cls, 0], X[Y==cls, 1], 1, marker=markers[index],
+                        c=cmaps(index), alpha=0.75, s=12.23, label=cls)
+
+            if test_idx:
+                x_test, y_test = X[test_idx, :], Y[test_idx]
+                curent_view.scatter(x_test[:, 0], x_test[:, 1], 1, linewidths=1, edgecolors="black", alpha=0.5, marker="o", label=f"test elem [{index}]")
+
+
+data = datasets.load_iris()
+X = data.data[:, [2, 3]]
+Y = data.target
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
+fnn = StandardScaler()
+fnn.fit(X_train)
+
+X_train_std = fnn.transform(X_train)
+X_test_std = fnn.transform(X_test)
+
+X_combined = np.vstack((X_train_std, X_test_std))
+Y_combined = np.hstack((Y_train, Y_test))
+
+smn = SVC(kernel="rbf", random_state=0, C=1.0)
+pnn = Perceptron(max_iter=40, eta0=0.01, random_state=0)
+log = LogisticRegression(C=1.0, random_state=0)
+
+log.fit(X_train_std, Y_train)
+pnn.fit(X_train_std, Y_train)
+smn.fit(X_train_std, Y_train)
+generate_desion(X_combined, Y_combined, 
+                classifier=smn, test_idx=range(105, 150), curent_view=axis_3d)
+generate_desion(X_combined, Y_combined, 
+                classifier=pnn, test_idx=range(105, 150), curent_view=axis_3d_01)
+generate_desion(X_combined, Y_combined, 
+                classifier=log, test_idx=range(105, 150), curent_view=axis_3d_02)
+
+plt.legend(loc="lower center")
 plt.show()
+
+
+
+
+
+
+
